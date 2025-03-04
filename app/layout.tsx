bcb13fae5cf0763fa2203360ba9730076b325e4e
sureboyhtml <ebukaj665@gmail.com>
2025-03-04 03:54:41 +0100

    new: setup react query

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import ReactQueryProvider from "@/lib/query-provider";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -8,7 +9,8 @@ const defaultUrl = process.env.VERCEL_URL
 export const metadata = {
   metadataBase: new URL(defaultUrl),
   title: "Flickmart",
-  description: "The all in one store for university students to buy and sell products",
+  description:
+    "The all in one store for university students to buy and sell products",
 };
 const inter = Inter({
   subsets: ["latin"],
@@ -21,7 +23,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
-      <body className="bg-background text relative">{children}</body>
+      <body className="bg-background text relative">
+        <ReactQueryProvider>{children}</ReactQueryProvider>
+      </body>
     </html>
   );
 }
