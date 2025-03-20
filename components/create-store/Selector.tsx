a4084cc89e23dd8e02637431797f95cf3cac1603
Raymond Ogbuehi <raymondogb@gmail.com>
2025-03-20 07:59:30 +0100

    feat: add phoneNumber field to create-store form

--- components/create-store/Selector.tsx
+++ components/create-store/Selector.tsx
@@ -6,13 +6,14 @@ import {
   SelectValue,
 } from "../ui/select";
 import React from "react";
-import { FormField } from "../ui/form";
-import { UseFormReturn} from "react-hook-form";
+import { FormField, FormItem, FormMessage } from "../ui/form";
+import { UseFormReturn } from "react-hook-form";
 
 interface FormType {
   businessName: string;
   location: string;
   address: string;
+  phoneNumber: string & { __tag: "E164Number" };
 }
 
 export default function Selector({
@@ -22,54 +23,57 @@ export default function Selector({
   form,
   className = "",
 }: {
-  name: "businessName" | "location" | "address";
+  name: "businessName" | "location" | "address" | "phoneNumber";
   options: Array<string>;
   label?: string;
   form: UseFormReturn<FormType>;
   className?: string;
 }) {
   return (
-    <div className="border border-gray-300 lg:py- py-2.5 rounded-lg">
-      <FormField
-        control={form.control}
-        name={name}
-        render={({ field }) => {
-          let value = "";
-          if (typeof field.value === "string") {
-            value = field.value;
-          }
-          return (
-            <Select
-              required
-              onValueChange={field.onChange}
-              {...field}
-              defaultValue={value}
-              value={value}
-            >
-              <SelectTrigger
-                className={`w-full  text-lg  capitalize outline-none  border-none shadow-none focus:ring-0 ${className}`}
+    <FormField
+      control={form.control}
+      name={name}
+      render={({ field }) => {
+        let value = "";
+        if (typeof field.value === "string") {
+          value = field.value;
+        }
+        return (
+          <FormItem>
+            <div className="border border-gray-300 lg:py- py-2.5 rounded-lg">
+              <Select
+                required
+                onValueChange={field.onChange}
+                {...field}
+                defaultValue={value}
+                value={value}
               >
-                <SelectValue placeholder={`${label || name}*`} />
-              </SelectTrigger>
-              <SelectContent
-                side="bottom"
-                align="center"
-                className=" py-3  capitalize  rounded-lg shadow-lg"
-              >
-                {options.map((item) => (
-                  <SelectItem
-                    className="hover:bg-slate-100 px-5 py-3 rounded-lg"
-                    value={item}
-                    key={item}
-                  >
-                    {item}
-                  </SelectItem>
-                ))}
-              </SelectContent>
-            </Select>
-          );
-        }}
-      />
-    </div>
+                <SelectTrigger
+                  className={`w-full  text-lg  capitalize outline-none  border-none shadow-none focus:ring-0 ${className}`}
+                >
+                  <SelectValue placeholder={`${label || name}*`} />
+                </SelectTrigger>
+                <SelectContent
+                  side="bottom"
+                  align="center"
+                  className=" py-3  capitalize  rounded-lg shadow-lg"
+                >
+                  {options.map((item) => (
+                    <SelectItem
+                      className="hover:bg-slate-100 px-5 py-3 rounded-lg"
+                      value={item}
+                      key={item}
+                    >
+                      {item}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+            </div>
+            <FormMessage />
+          </FormItem>
+        );
+      }}
+    />
   );
 }
