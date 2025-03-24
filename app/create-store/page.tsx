1e071a6f88fd3eafe39ce9b2266d01dbe89650d2
Raymond Ogbuehi <raymondogb@gmail.com>
2025-03-24 22:32:06 +0100

    refactor: use mobile header component in create store page

--- app/create-store/page.tsx
+++ app/create-store/page.tsx
@@ -4,20 +4,16 @@ import StageOne from "@/components/create-store/StageOne";
 import StageTwo from "@/components/create-store/StageTwo";
 import StageThree from "@/components/create-store/StageThree";
 import StageFour from "@/components/create-store/StageFour";
-import { ChevronLeft } from "lucide-react";
 import Image from "next/image";
 import { useState } from "react";
+import MobileHeader from "@/components/MobileHeader";
+
 const page = () => {
   const [stage, setStage] = useState<1 | 2 | 3 | 4>(1);
   const [avatar, setAvatar] = useState<string | null>("");
   return (
     <>
-      <header className="shadow-lg py-[24px] container-px sticky top-0 bg-white sm:hidden">
-        <button className="flex font-light items-center transition-colors text-flickmart-gray hover:text-flickmart duration-300 text-sm">
-          <ChevronLeft size={30} strokeWidth={1.5} />
-          Create Store
-        </button>
-      </header>
+    <MobileHeader title="Create Store" />
       <main
         className={`px-4 pb-10 text-center max-w-[500px] mt-[56px] mx-auto ${stage >= 2 ? "md:grid md:grid-cols-2 md:max-w-[900px] w-full md:gap-16 md:items-center md:fixed md:top-1/2 md:left-1/2 md:-translate-x-1/2 md:-translate-y-1/2" : ""}`}
       >
