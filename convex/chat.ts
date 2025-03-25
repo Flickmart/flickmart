de1227ef120480924aa286803d703a21109030a7
sureboyhtml <ebukaj665@gmail.com>
2025-03-25 12:23:32 +0100

    run convex deploy

---
+++ convex/chat.ts
@@ -0,0 +1,12 @@
+import {  query,mutation  } from "./_generated/server";
+import { v, Validator } from "convex/values";
+
+
+
+export const sendMessage = query({
+  
+
+    handler: ()=>{
+
+    }
+})
