9a00b303bae7bea56bfdb0f81814a21d56d0f5bf
sureboyhtml <ebukaj665@gmail.com>
2025-03-28 00:12:11 +0100

    remove reacy query

--- app/post-ad/page.tsx
+++ app/post-ad/page.tsx
@@ -1,13 +1,10 @@
 "use client";
 import PostAdForm from "@/components/post-ad/PostAdForm";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 
 export default function Page() {
-  const queryClient = new QueryClient();
 
   return (
-    <QueryClientProvider client={queryClient}>
       <div className="bg-gray-100 p-3  lg:p-10 min-h-screen space-y-5 flex flex-col justify-center items-center">
         <div className="bg-white lg:w-5/6 w-full h-20 rounded-lg flex items-center justify-between">
           <span className="text-xl text-gray-700 font-semibold pl-7">
@@ -19,6 +16,5 @@ export default function Page() {
         </div>
         <PostAdForm />
       </div>
-    </QueryClientProvider>
   );
 }
