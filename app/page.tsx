2874827f34b9ed146388115089f87da663060bb2
egentinnamdi <egentinnamdi10@gmail.com>
2025-03-28 15:18:35 +0100

    minor fix to image preview post ad page

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,14 @@
 import { SignedIn, SignedOut, UserButton, SignInButton } from "@clerk/nextjs";
 
 export default function Home() {
+  const redirectSignIn= process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL
   return (
     <div className="flex gap-3">
       <SignedIn>
         <UserButton />
       </SignedIn>
       <SignedOut>
-        <SignInButton mode="modal" />
+        <SignInButton mode="modal"  fallbackRedirectUrl={redirectSignIn}/>
       </SignedOut>
       <span>Flickmart landing page</span>
     </div>
