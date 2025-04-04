0c34925533e113005ac9e93f36009adc0a6a57f5
adgdevv <opponamer@gmail.com>
2025-04-04 07:07:45 +0100

    changed the btn in saved and wishlist

--- components/NavigationBar.tsx
+++ components/NavigationBar.tsx
@@ -20,7 +20,7 @@ const NavigationBar = () => {
             <div className="w-[98%] mx-auto">
                 <button onClick={()=> router.back()} className="flex font-light items-center transition-colors text-flickmart-gray hover:text-flickmart duration-300 capitalize text-sm">
                     <ChevronLeft size={35} strokeWidth={1.5} />
-                    <span className={isPage ? 'block':'hidden'}>Find&nbsp;</span> {getLastPathSegment(cPath)}<span className="hidden lg:block">&nbsp;In Nigeria.</span>
+                    <span className={isPage ? 'block':'hidden'}>Find&nbsp;</span> {getLastPathSegment(cPath)}<span className="hidden lg:block">&nbsp; {isPage ? "In Nigeria." : ''} </span>
                 </button>
             </div>
             
