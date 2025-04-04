256503e091512fe502e622f43783699ee540454d
sureboyhtml <ebukaj665@gmail.com>
2025-04-04 11:35:00 +0100

    fix the auth domain

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://guided-lemming-20.clerk.accounts.dev",
+      domain: "https://lasting-gar-39.clerk.accounts.dev",
       applicationID: "convex",
     },
   ],
