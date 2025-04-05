565e7e83bcced6ace116bbc5cf0c2d0024c59d7a
Raymond Ogbuehi <raymondogb@gmail.com>
2025-04-05 14:51:21 +0100

    style: add animation

--- app/(main-pages)/settings/products/page.tsx
+++ app/(main-pages)/settings/products/page.tsx
@@ -8,7 +8,6 @@ import {
   BreadcrumbPage,
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
-import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
 import { SidebarTrigger, useSidebar } from "@/components/ui/sidebar";
 import clsx from "clsx";
@@ -21,7 +20,14 @@ import {
   X,
 } from "lucide-react";
 import Image from "next/image";
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import AnimatedSearchBar from "@/components/AnimatedSearchBar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 const demoProducts = [
   {
@@ -46,23 +52,9 @@ const demoProducts = [
     image: "/car.jpeg",
   },
 ];
-interface ProductMenuState {
-  id: number;
-  isOpen: boolean;
-}
 
 export default function ProductsPage() {
   const { isMobile, openMobile, setOpenMobile } = useSidebar();
-  const [productsMenuStates, setProductsMenuStates] = useState<
-    ProductMenuState[]
-  >([]);
-  const [searchBarIsVisible, setSearchBarIsVisible] = useState(false);
-
-  useEffect(() => {
-    setProductsMenuStates(
-      demoProducts.map((product) => ({ id: product.id, isOpen: false }))
-    );
-  }, []);
 
   const formatDesc = (desc: string) => {
     if (desc.length > 50) {
@@ -70,6 +62,7 @@ export default function ProductsPage() {
     }
     return desc;
   };
+  const [isExpanded, setIsExpanded] = useState(false);
 
   return (
     <div className="flex flex-col gap-8 p-4 lg:px-10">
@@ -97,25 +90,21 @@ export default function ProductsPage() {
           </BreadcrumbList>
         </Breadcrumb>
       </header>
-      {/* <div className="grid gap-4 md:grid-cols-3">
-        <div className="rounded-xl bg-muted/50 p-6">
-          <h3 className="text-lg font-semibold">Total Products</h3>
-          <p className="text-3xl font-bold mt-2">1,234</p>
-        </div>
-        <div className="rounded-xl bg-muted/50 p-6">
-          <h3 className="text-lg font-semibold">Out of Stock</h3>
-          <p className="text-3xl font-bold mt-2">12</p>
-        </div>
-        <div className="rounded-xl bg-muted/50 p-6">
-          <h3 className="text-lg font-semibold">Low Stock</h3>
-          <p className="text-3xl font-bold mt-2">45</p>
-        </div>
-      </div> */}
-      <div className={clsx("flex items-center justify-between h-20 sm:h-32")}>
+      <div
+        className={clsx(
+          "flex relative items-center justify-between h-20 sm:h-32",
+          {
+            "!justify-end": isExpanded,
+          }
+        )}
+      >
         <div
-          className={clsx("flex items-center gap-3", {
-            "hidden lg:flex": searchBarIsVisible,
-          })}
+          className={clsx(
+            "flex flex-grow items-center gap-3 transition-opacity duration-300 relative",
+            {
+              "opacity-0 lg:flex": isExpanded,
+            }
+          )}
         >
           <div className="relative size-20 sm:size-32">
             <Image src="/store-avatar.png" alt="avatar" fill />
@@ -128,93 +117,65 @@ export default function ProductsPage() {
             </p>
           </div>
         </div>
-        <div
-          className={clsx(" rounded-md overflow-hidden", {
-            "grid grid-cols-[1fr_40px] flex-grow bg-gray-200 h-11 transition-[border] border-[1.5px] focus-within:border-flickmart lg:flex-none":
-              searchBarIsVisible,
-          })}
+        <AnimatedSearchBar
+          placeholder="Search for anything..."
+          isExpanded={isExpanded}
+        />
+        <button
+          onClick={() => {
+            setIsExpanded(!isExpanded);
+          }}
+          type="button"
+          className="absolute right-2 hover:text-flickmart"
         >
-          {searchBarIsVisible && (
-            <Input className="bg-transparent  h-full" placeholder="Search..." />
+          {isExpanded ? (
+            <X className="size-6 sm:size-8 transition-all duration-300" />
+          ) : (
+            <Search className="size-6 sm:size-8 transition-all duration-300" />
           )}
-          <button
-            onClick={() => {
-              setSearchBarIsVisible(!searchBarIsVisible);
-            }}
-            className="flex items-center justify-center"
-          >
-            {searchBarIsVisible ? (
-              <X className="text-[#757474C2] hover:text-flickmart transition-colors duration-300 sm:size-7" />
-            ) : (
-              <Search className="text-[#757474C2] hover:text-flickmart transition-colors duration-300 sm:size-7" />
-            )}
-          </button>
-        </div>
+        </button>
       </div>
       <div>
         <ul className="grid gap-4">
           {demoProducts.map((product) => (
             <li
               key={product.id}
-              className="flex gap-3 bg-white p-[6px] rounded-sm items-center shadow-md relative sm:p-2"
+              className="flex justify-between bg-white p-[6px] rounded-sm items-center shadow-md relative sm:p-2"
             >
-              <div className="size-20 sm:size-28 relative flex-shrink-0">
-                <Image
-                  className="object-cover rounded-sm"
-                  fill
-                  src={product.image}
-                  alt={product.name}
-                ></Image>
-              </div>
-              <div>
-                <h2 className="sm:text-lg font-bold">{product.name}</h2>
-                <p className="text-xs sm:text-sm font-light mb-1 sm:mb-2 md:mb-4">
-                  {formatDesc(product.desc)}
-                </p>
-                <span className="text-xs sm:text-sm font-bold text-flickmart">
-                  &#8358;{product.price}
-                </span>
-              </div>
-              <button
-                onClick={() => {
-                  setProductsMenuStates((prev) =>
-                    prev.map((state) =>
-                      state.id === product.id
-                        ? { ...state, isOpen: !state.isOpen }
-                        : { ...state, isOpen: false }
-                    )
-                  );
-                }}
-                className="absolute top-3 right-1"
-                type="button"
-              >
-                <EllipsisVertical className="hover:text-flickmart transition-all duration-300 size-4 sm:size-5 md:size-6" />
-              </button>
-              {productsMenuStates.find((state) => state.id === product.id)
-                ?.isOpen && (
-                <div className="absolute top-8 right-2 bg-white text-sm shadow-[0_3px_10px_#00000040] rounded-sm font-semibold overflow-hidden sm:text-sm sm:top-10">
-                  <ul className="flex flex-col">
-                    <li>
-                      <button
-                        className="hover:bg-gray-200 transition-all duration-300 flex flex-row items-center gap-2 w-full px-2 py-1 sm:p-2"
-                        type="button"
-                      >
-                        <Pencil className="size-4 sm:size-[18px]" />
-                        <span>Edit</span>
-                      </button>
-                    </li>
-                    <li>
-                      <button
-                        className="hover:bg-gray-200 transition-all duration-300 flex flex-row items-center gap-2 w-full px-2 py-1 sm:p-2 text-red-600"
-                        type="button"
-                      >
-                        <Trash className="size-4 sm:size-[18px]" />
-                        <span>Delete</span>
-                      </button>
-                    </li>
-                  </ul>
+              <div className="flex gap-3 items-center">
+                <div className="size-20 sm:size-28 relative flex-shrink-0">
+                  <Image
+                    className="object-cover rounded-sm"
+                    fill
+                    src={product.image}
+                    alt={product.name}
+                  ></Image>
                 </div>
-              )}
+                <div>
+                  <h2 className="sm:text-lg font-bold">{product.name}</h2>
+                  <p className="text-xs sm:text-sm font-light mb-1 sm:mb-2 md:mb-4">
+                    {formatDesc(product.desc)}
+                  </p>
+                  <span className="text-xs sm:text-sm font-bold text-flickmart">
+                    &#8358;{product.price}
+                  </span>
+                </div>
+              </div>
+              <DropdownMenu>
+                <DropdownMenuTrigger>
+                  <EllipsisVertical className="cursor-pointer" />
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="w-28 min-w-0">
+                  <DropdownMenuItem>
+                    <Pencil className="mr-2" />
+                    Edit
+                  </DropdownMenuItem>
+                  <DropdownMenuItem className="!text-red-500">
+                    <Trash className="mr-2" />
+                    Delete
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
             </li>
           ))}
         </ul>
