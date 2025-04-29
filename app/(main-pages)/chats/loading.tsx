afe402958e6ecc4ae3cc0d8876ad562a5db45e0c
sureboyhtml <ebukaj665@gmail.com>
2025-04-29 23:56:40 +0100

    refactor: replace MoonLoader with ClimbingBoxLoader for improved loading animation

--- app/(main-pages)/chats/loading.tsx
+++ app/(main-pages)/chats/loading.tsx
@@ -1,9 +1,9 @@
+import { ClimbingBoxLoader } from "react-spinners";
 
-
- const LoadingPage = ()=>{
-    return (
-        <div>Loading...</div>
-    )
+export default function loadingChat() {
+  return (
+    <div className="flex justify-center items-center w-screen h-screen">
+      <ClimbingBoxLoader color="#f81" />
+    </div>
+  );
 }
-
-export default LoadingPage;
--- app/(main-pages)/loading.tsx
+++ app/(main-pages)/loading.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { MoonLoader } from "react-spinners";
+import { ClimbingBoxLoader } from "react-spinners";
 
 export default function loading() {
   return (
     <div className="bg-black/50 flex justify-center items-center z-50 fixed  inset-0">
-      <MoonLoader  />
+      <ClimbingBoxLoader color="#f81"/>
     </div>
   );
 }
