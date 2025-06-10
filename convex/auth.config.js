40e397dbe5247379c47a4537fe5ee34682bf47fc
egentinnamdi <egentinnamdi10@gmail.com>
2025-06-10 22:38:41 +0100

    .

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -2,9 +2,9 @@ export default {
   providers: [
     {
       // Production
-      // domain: "https://lasting-gar-39.clerk.accounts.dev",
+      domain: "https://lasting-gar-39.clerk.accounts.dev",
       // Development
-      domain: "https://guided-lemming-20.clerk.accounts.dev",
+      // domain: "https://guided-lemming-20.clerk.accounts.dev",
       applicationID: "convex",
     },
   ],
