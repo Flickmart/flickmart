bfbebde99373ec7248b1f90ebd3051fcfa3de51b
egentinnamdi <egentinnamdi10@gmail.com>
2025-07-04 15:48:13 +0100

    .

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -3,6 +3,7 @@ export default {
     {
       // Production
       domain: "https://clerk.flickmart.app",
+
       // Development
       // domain: "https://lasting-gar-39.clerk.accounts.dev",
       applicationID: "convex",
