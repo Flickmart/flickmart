b37c72e82f4d587b9cbdd9a464a570454c80ac37
egentinnamdi <egentinnamdi10@gmail.com>
2025-07-04 15:33:04 +0100

    fix(auth): enable production domain for clerk authentication

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -2,7 +2,7 @@ export default {
   providers: [
     {
       // Production
-      // domain: "https://clerk.flickmart.app",
+      domain: "https://clerk.flickmart.app",
       // Development
       // domain: "https://lasting-gar-39.clerk.accounts.dev",
       applicationID: "convex",
