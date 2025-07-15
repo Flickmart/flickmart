0cce81f7d69123efe5178f7566e59a9d8794757f
flickamrt28 <flickmart2024@gmail.com>
2025-07-15 19:41:30 +0100

    .

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -3,6 +3,7 @@ export default {
     {
       // Production
       domain: "https://clerk.flickmart.app",
+
       // Development
       // domain: "https://guided-lemming-20.clerk.accounts.dev",
       applicationID: "convex",
