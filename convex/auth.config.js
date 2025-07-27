70ef45ee840d2d09f61726248528a5b4c77866bc
Joseph Ebuka <ebukaj665@gmail.com>
2025-07-27 02:43:11 +0100

    auth

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,12 +1,7 @@
 export default {
   providers: [
     {
-      // Production
-       domain: "https://clerk.flickmart.app",
-      // Development
-     // domain: "https://lasting-gar-39.clerk.accounts.dev"
-      // domain: "https://guided-lemming-20.clerk.accounts.dev",
-
+      domain: process.env.NEXT_PUBLIC_CLERK_SERVER_CONFIG,
       applicationID: "convex",
     },
   ],
