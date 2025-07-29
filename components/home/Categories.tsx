ad5318713db9f92933b6cf7f1b4d8329663072cc
FlexCodes <egentinnamdi10@gmail.com>
2025-07-29 10:31:57 +0100

    Update Categories.tsx

--- components/home/Categories.tsx
+++ components/home/Categories.tsx
@@ -20,7 +20,7 @@ const categoryItems: Array<CategoryObj> = [
 
 export default function Categories() {
   return (
-    <Container className={"pt-4 sm:pt-8 pb-4 !min-h-[35vh] sm:!min-h-[50vh]"}>
+<Container className={"pt-4 sm:pt-8 pb-4 !min-h-[35vh] sm:!min-h-[50vh]"}>
       <div className="grid grid-cols-4 grid-rows-3 w-full gap-x-[10px] gap-y-2 lg:w-4/6">
         {categoryItems.map((item) => (
           <CategoryItem
