385adcd5e58bcb9212f9b7e82aba6e27e1ce0d0b
flickamrt28 <flickmart2024@gmail.com>
2025-07-29 11:19:02 +0100

    fixed home category item

--- components/home/CategoryItem.tsx
+++ components/home/CategoryItem.tsx
@@ -8,9 +8,9 @@ export default function CategoryItem({
   categoryName: string;
 }) {
   return (
-    <Link href={`/subcategories`}>
-      <div className="hover:cursor-pointer">
-        <div className="bg-[#f4f7fa] lg:p-0  h-36 lg:h-52 lg:rounded-xl capitalize flex flex-col items-center justify-center space-y-4 lg:space-y-7 text-gray-800">
+    <Link href={`/categories/${categoryName}`} className="block">
+      <div className="hover:cursor-pointer flex flex-col items-center gap-[6px] h-full md:gap-2">
+        <div className="bg-[#f4f7fa] rounded-lg capitalize flex flex-col w-full h-full items-center justify-center lg:rounded-xl">
           <Image
             src={`/${categoryName}.png`}
             alt={categoryName}
@@ -19,7 +19,9 @@ export default function CategoryItem({
             className="h-1/3 w-3/4 object-contain "
           />
         </div>
-        <span className="font-bold text-sm lg:text-xl">{categoryName}</span>
+        <span className="font-bold text-gray-800 text-[13px] capitalize sm:text-sm md:text-base xl:text-xl">
+          {categoryName}
+        </span>
       </div>
     </Link>
   );
