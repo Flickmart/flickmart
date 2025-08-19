2a03b5f7c72df7517082693d6f5344abe31e4cdc
Joseph Ebuka <ebukaj665@gmail.com>
2025-08-19 01:03:25 +0100

    one signal init

--- app/(main-pages)/page.tsx
+++ app/(main-pages)/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useState , useEffect} from "react";
 import BestSellers from "@/components/home/BestSellers";
 import Categories from "@/components/home/Categories";
 import NewArrivals from "@/components/home/NewArrivals";
@@ -8,13 +9,24 @@ import Delivery from "@/components/Delivery";
 import SearchBox from "@/components/SearchBox";
 import MobileNav from "@/components/MobileNav";
 import SearchOverlay from "@/components/SearchOverlay";
-import { useState } from "react";
+import OneSignal from 'react-onesignal';
 
 export default function Home() {
   const [searchOpen, setSearchOpen] = useState(false);
   function openSearch(val: boolean) {
     setSearchOpen(val);
   }
+
+  useEffect(() => {
+    // Ensure this code runs only on the client side
+    if (typeof window !== 'undefined') {
+      OneSignal.init({
+        appId: "1602d5cc-611d-46a2-a87f-0d711c6e7f99",
+        safari_web_id: "web.onesignal.auto.38b1a4de-a361-440e-ae28-b71c05790af2",
+      });
+    }
+  }, []);
+
   return (
     <>
       <MobileNav />
