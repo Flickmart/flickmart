173309cd4d7d35149091a252426a54140fbf7a62
Joseph Ebuka <ebukaj665@gmail.com>
2025-08-19 02:32:46 +0100

    new: setup posthog

--- next.config.js
+++ next.config.js
@@ -33,6 +33,24 @@ const baseConfig = {
       },
     ],
   },
+  async rewrites() {
+    return [
+      {
+        source: "/ingest/static/:path*",
+        destination: "https://us-assets.i.posthog.com/static/:path*",
+      },
+      {
+        source: "/ingest/:path*",
+        destination: "https://us.i.posthog.com/:path*",
+      },
+      {
+        source: "/ingest/flags",
+        destination: "https://us.i.posthog.com/flags",
+      },
+    ];
+  },
+  // This is required to support PostHog trailing slash API requests
+  skipTrailingSlashRedirect: true,
 };
 
 // Sentry config
