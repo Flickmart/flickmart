a4a3096b7fbb8f2cc78c825e4dc0fb5f6901c6f6
Joseph Ebuka <ebukaj665@gmail.com>
2025-11-12 13:58:29 +0100

    simple format

--- components/home/Categories.tsx
+++ components/home/Categories.tsx
@@ -4,10 +4,10 @@ import Container from './Container';
 
 export default function Categories() {
   return (
-    <section className="sm:mt-2 lg:fixed lg:w-[25%] lg:left-0 lg:top-[67px]">
+    <section className="sm:mt-2 lg:fixed lg:top-[67px] lg:left-0 lg:w-[25%]">
       <h2 className="section-title mb-0 md:mb-4 lg:hidden">Categories</h2>
-      <Container className={"!min-h-0"}>
-        <div className="grid grid-cols-5 lg:flex lg:flex-col lg:w-full lg:pl-[30px]">
+      <Container className={'!min-h-0'}>
+        <div className="grid grid-cols-5 lg:flex lg:w-full lg:flex-col lg:pl-[30px]">
           {categoryItems.map((item) => (
             <CategoryItem
               categoryName={item.categoryName}
